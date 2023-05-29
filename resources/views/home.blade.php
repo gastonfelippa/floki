b114c45158dc63a318cea50f37d62cbb885a030a
gaston_felippa@hotmail.com <gaston_felippa@hotmail.com>
2023-05-28 23:42:40 -0300

    sin saludo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,13 +10,13 @@
     <div class="col-12 layout-spacing">
         <div class="widget-content-area">
             <div class="widget-one"> 
-            @include('common.alerts')               
+            <!-- @include('common.alerts')               
                 @if(Auth::user()->sexo == 1)
                     <h1 class="text-center">Bienvenida <strong>{{Auth::user()->name}}!!!</strong></h1>
                 @else
                     <h1 class="text-center">Bienvenido <strong>{{Auth::user()->name}}!!!</strong></h1>
                 @endif             
-            </div>
+            </div> -->
             <!-- <div class="visible-print text-center">
                 {!! QrCode::size(100)->generate("www.floki.ar/productos") !!}
                 <p>Escanéame para hacer tu pedido.</p>
